65a10aa2a533d4afd03ec7dd240c8e90c35008cd
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2026-06-27 19:41:42 -0300

    fix(#46): adiciona associations disciplina e docente ao model Turma

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,6 +1,8 @@
 # app/models/turma.rb
 class Turma < ApplicationRecord
   belongs_to :departamento
+  belongs_to :disciplina, optional: true
+  belongs_to :docente, optional: true
 
   validates :codigo, presence: true
   validates :nome, presence: true
