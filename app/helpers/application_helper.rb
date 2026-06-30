980114b94abcd06a712613f17a4886ea18e4dd08
Anarayssa-dev <anarayssa.dev@gmail.com>
2026-06-30 19:24:44 -0300

    Rdoc em app/helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
-module ApplicationHelper
+## Modulo de helper para application
+##
+  # === Descrição
+  # Modulo padrão no rails para métodos auxiliares
+  # === Argumentos
+  # * Nenhum.
+  # === Retorno
+  # * +nil+
+  # === Efeitos Colaterais
+  # * Nenhum.
+module ApplicationHelpe
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
+## Modulo de helper para sessions
+##
+  # === Descrição
+  # Modulo padrão no rails para métodos auxiliares
+  # === Argumentos
+  # * Nenhum.
+  # === Retorno
+  # * +nil+
+  # === Efeitos Colaterais
+  # * Nenhum.
 module SessionsHelper
 end
