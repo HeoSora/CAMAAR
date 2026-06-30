78fa4b1c4c6d67faef038a58f71f9198fec37692
Anarayssa-dev <anarayssa.dev@gmail.com>
2026-06-30 20:00:53 -0300

    Rdoc em app/mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,13 @@
+## Classe base para os mailers do rails
+##
+  # === Descrição
+  # Classe que define o remetente <tt>ES2026_Grupo01@camaar.com</tt> e layout padrão de e-mails
+  # === Argumentos
+  # * Nenhum.
+  # === Retorno
+  # * +nil+
+  # === Efeitos Colaterais
+  # * Nenhum.
 class ApplicationMailer < ActionMailer::Base
   default from: "ES2026_Grupo01@camaar.com"
   layout "mailer"
